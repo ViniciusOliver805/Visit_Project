50b44da0cb7266a3c2e48701a6c57ff69cda2eac
Vinicius Oliver <viniciusoliver805@gmail.com>
2024-09-09 22:50:59 -0400

    att database

--- backend/backend-env/config.py
+++ backend/backend-env/config.py
@@ -1,8 +1,8 @@
 import os
 
 DATABASE = {
-    'host': os.getenv ('localhost'),
-    'user': os.getenv ('root'),
-    'password': os.getenv ('root'),
-    'database': os.getenv ('bdvisita')
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_NAME')
 }
